74c2d187b2e4eec1ac6c2e1f60f7233e11ded9dc
nombre <daniel.b100726@gmail.com>
2021-09-05 21:22:58 -0400

    Creando BD, parte 1

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.get('/', (require, response) => {//El manejador '*' en el get, quiere decir que va a estar manejando absolutamente cualquier ruta que le llegue
-  console.log('Hello World');
-  response.send({ message: 'Response' });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParsel: true, useUndefinedTopology: true });
+
+const Users = mongoose.model("Users", new mongoose.Schema({ name: String }))
+
+Users.create({ name: 'New String Message' })
+
+app.get('*', (require, response) => {
+  //El manejador '*' en el get, quiere decir que va a estar manejando absolutamente cualquier ruta que le llegue
+  Users.find()
+  .then(x => response.send(x))
 });
 
 module.exports = app;
