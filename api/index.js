822d628b51af5fbc72f794c2990f9077642840f4
nombre <daniel.b100726@gmail.com>
2021-09-11 18:50:23 -0400

    Creando BD, parte 4, finalmente se conecta

--- api/index.js
+++ api/index.js
@@ -2,16 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const Users = mongoose.model("Users", new mongoose.Schema({ name: String }))
+const Users = mongoose.model('Users', new mongoose.Schema({ name: String }));
 
-Users.create({ name: 'Finalmente se conecta' })
+Users.create({ name: 'Finalmente se conecta' });
 
 app.get('*', (require, response) => {
   //El manejador '*' en el get, quiere decir que va a estar manejando absolutamente cualquier ruta que le llegue
-  Users.find()
-  .then(x => response.send(x))
+  Users.find().then((x) => response.send(x));
 });
 
 module.exports = app;
