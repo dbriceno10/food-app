e17e851323d6da517f75742ba01201e0b0cf222f
nombre <daniel.b100726@gmail.com>
2021-09-12 00:00:01 -0400

    Agregando cors

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const meals = require('./routes/meals');
 const orders = require('./routes/orders');
 const app = express();
-app.use(bodyParser.json());//Indicamos que use body-parser, va a tomar lo que enviemos al bory y lo va a convertir en json
-// const router = express.Router();
+app.use(bodyParser.json()); //Indicamos que use body-parser, va a tomar lo que enviemos al bory y lo va a convertir en json
+app.use(cors());//Esto nos va a permitir acceder a nuestra aplicación de manera fácil desde cualquier entorno fuera de localhost
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
