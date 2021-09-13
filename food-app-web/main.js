f68011e322e4230a3e0cb21af0bb03cd30051240
nombre <daniel.b100726@gmail.com>
2021-09-13 17:18:28 -0400

    Añadiendo templates, parte 1

--- food-app-web/main.js
+++ food-app-web/main.js
@@ -1,9 +1,18 @@
 // window.onload = () => {};
+const renderItem = (item) => {
+  // return `<li>${item}</li>`
+  return `<li data-id=${item._id}>${item.name}</li>`
+}
+
+
 const UrlApi = 'https://food-app-dbriceno10.vercel.app/api/meals';
 fetch(UrlApi)
   .then((response) => response.json())
   .then((data) => {
     const mealsList = document.getElementById('meals-list');
-    const template = data.map((element) => `<li>${element.name}</li>`).join('');
+    const submit = document.getElementById('submit');
+    // const template = data.map(element => renderItem(element.name)).join('');
+    const template = data.map(renderItem).join('');
     mealsList.innerHTML = template;
+    submit.removeAttribute('disabled')
   });
