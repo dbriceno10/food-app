bbbfce6ceb3b1ff93174177899be41f74bd04253
nombre <daniel.b100726@gmail.com>
2021-09-13 13:39:06 -0400

    repaso de fetch, parte3. Mostrando los datos traidos en un template

--- food-app-web/main.js
+++ food-app-web/main.js
@@ -2,4 +2,8 @@
 const UrlApi = 'https://food-app-dbriceno10.vercel.app/api/meals';
 fetch(UrlApi)
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    const mealsList = document.getElementById('meals-list');
+    const template = data.map((element) => `<li>${element.name}</li>`).join('');
+    mealsList.innerHTML = template;
+  });
