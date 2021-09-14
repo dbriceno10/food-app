3a34b7eb7556c6b367535e8a6a3369b7bd7840c4
nombre <daniel.b100726@gmail.com>
2021-09-14 12:13:19 -0400

    Mostrando ordenes desde el servidor

--- food-app-web/main.js
+++ food-app-web/main.js
@@ -7,10 +7,8 @@ const stringToHTML = (string) => {
   const child = document.body.firstChild; //fristChild es el primer elemento que se encuentra en nuestra etiqueta de body, es decir, el elemento li que estamos imprimiendo
   return child;
 };
-const renderItem = (item) => {
-  const view = `<li data-id=${item._id}>${item.name}</li>`;
-  const element = stringToHTML(view);
 
+const addClickEventToMeals = (element, meal) => {
   element.addEventListener('click', () => {
     const mealsList = document.getElementById('meals-list');
     //children nos regresa una colección de onjetos html, el cual se parece a un arreglo pero no lo es, se puede iterar (tiene índices). No tiene los métodos forEach y map
@@ -18,8 +16,21 @@ const renderItem = (item) => {
     arrayMealsList.forEach((element) => element.classList.remove('selected'));
     element.classList.add('selected');
     const mealsIdImput = document.getElementById('meals-id'); //el input de tipo hidden lo vamos a utilizar para capturar el id del meal
-    mealsIdImput.value = item._id;
+    mealsIdImput.value = meal._id;
   });
+};
+
+const renderMeals = (meal) => {
+  const view = `<li data-id=${meal._id}>${meal.name}</li>`;
+  const element = stringToHTML(view);
+  addClickEventToMeals(element, meal);
+  return element;
+};
+
+const renderOrder = (order, meals) => {
+  const meal = meals.find((elementMeal) => elementMeal._id === order.meal_id);
+  const view = `<li data-id=${order._id}>${meal.name} - ${order.user_id}</li>`;
+  const element = stringToHTML(view);
   return element;
 };
 
@@ -40,7 +51,7 @@ window.onload = () => {
     }
     const order = {
       meal_id: mealIdValue,
-      user_id: 'id_de_prueba',
+      user_id: 'id_de_prueba2',
     };
 
     fetch(UrlApiOrders, {
@@ -54,12 +65,23 @@ window.onload = () => {
 
   fetch(UrlApiMeals)
     .then((response) => response.json())
-    .then((data) => {
+    .then((dataMeals) => {
       const mealsList = document.getElementById('meals-list');
       const submit = document.getElementById('submit');
-      const listItems = data.map(renderItem);
+      const listMealsItems = dataMeals.map(renderMeals);
       mealsList.removeChild(mealsList.firstElementChild); //Remove child recibe como argumento un elemento html, le pasamos el primer elemento hijo (Loading...) en este caso para eliminarlo
-      listItems.forEach((element) => mealsList.appendChild(element));
+      listMealsItems.forEach((element) => mealsList.appendChild(element));
       submit.removeAttribute('disabled');
+      fetch(UrlApiOrders)
+        .then((response) => response.json())
+        .then((dataOrders) => {
+          const ordersList = document.getElementById('orders-list');
+          const listOrders = dataOrders.map((orderData) =>
+            renderOrder(orderData, dataMeals)
+          );
+          ordersList.removeChild(ordersList.firstElementChild);
+          listOrders.forEach((element) => ordersList.appendChild(element));
+          console.log(dataOrders);
+        });
     });
 };
